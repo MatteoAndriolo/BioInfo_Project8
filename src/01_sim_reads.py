73f1fbd6b0ca3753e83375baa6c40942b09dfb94
AndrioloMatteo <matteo.andriolo.2@studenti.unipd.it>
2022-05-05 00:15:23 +0200

    fixes

--- src/01_sim_reads.py
+++ src/01_sim_reads.py
@@ -5,24 +5,30 @@ import os
 from pathlib import Path
 
 # support data
-dir_reads=Path(os.path.join(READS_DIR, "reads50_simlord"))
-dir_reads.mkdir(parents=True, exist_ok=True)
-path_metadata_reads=os.path.join(dir_reads,"metadata.json")
 metadata_ref=json.load(open(os.path.join(REF_DIR,"metadata.json")))
 jsonData={}
 
+'''
+    SimLoRD parameters
+'''
+c=20
+pi=0.11
+pd=0.4
+ps=0.01
+tag=f"c{c}pi{pi}pd{pd}ps{ps}"
+directory_reads=Path(READS_DIR)/"reads50_simlord"/tag
+directory_reads.mkdir(parents=True, exist_ok=True)
+path_metadata_reads=directory_reads/"metadata.json"
 
-# def function generate data for multithreading
+'''
+    Generate reads using one fna and a configuration file
+'''
 def gen_read(data):
     name=data[0]
-    metadata=data[1]
+    fref=data[1]
     # SimLoRD parameters
-    c=20
-    pi=0.11
-    pd=0.4
-    ps=0.01
-    fref=metadata["path"]
-    fread=dir_reads/name
+    fread=directory_reads/(name+".fastq")
+    
     # execute SimLoRD
     os.system(f'echo "simlord --read-reference {fref} -c {c} -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}"')
     os.system(f'simlord --read-reference {fref} -c 20 -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}')
@@ -30,30 +36,27 @@ def gen_read(data):
     return (name+'.fastq', {
         "path":str(fread),
         'path_ref':str(fref),
-        "c":c,
-        'pi':pi,
-        'pi':pi,
-        'pd':pd,
-        'ps':ps,
+        'tag':tag
     })
 
 # multitread read simiojn
-with Pool(os.cpu_count()*2) as p:
-    for par in p.map(gen_read, metadata_ref.items()):
-        jsonData[par[0]]=par[1]
+if __name__=="__main__":
+    with Pool(os.cpu_count()*2) as p:
+        for par in p.map(gen_read, [(v,k["path"]) for v,k in metadata_ref.items()]):
+            jsonData[par[0]]=par[1]
 
-""" iterative  with no metadata
-for name in metadata:
-    fref=metadata[name]["path"]
-    fread=dir_reads/name
-    pi=0.11
-    pd=0.4
-    ps=0.01
-    
-    os.system(f'echo "simlord --read-reference {fref} -c 20 -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}"')
-    os.system(f'simlord --read-reference {fref} -c 20 -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}')
-"""
+    """ iterative  with no metadata
+    for name in metadata:
+        fref=metadata[name]["path"]
+        fread=dir_reads/name
+        pi=0.11
+        pd=0.4
+        ps=0.01
+        
+        os.system(f'echo "simlord --read-reference {fref} -c 20 -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}"')
+        os.system(f'simlord --read-reference {fref} -c 20 -pi {pi} -pd {pd} -ps {ps} --no-sam {fread}')
+    """
 
 
-with open(path_metadata_reads, "w") as f:
-    json.dump(jsonData, f, indent=4)
+    with open(path_metadata_reads, "w") as f:
+        json.dump(jsonData, f, indent=4)
--- src/02_construct_FASTA.py
+++ src/02_construct_FASTA.py
@@ -1,23 +1,29 @@
 import json
 import os, shutil
+from pathlib import Path
+from definitions import READS_DIR
 
+c=20
+pi=0.11
+pd=0.4
+ps=0.01
+tag=f"c{c}pi{pi}pd{pd}ps{ps}"
 # support data
-metadata_reads=json.load(open("../reads/reads_50/metadata.json"))
-dir_reads="../reads/reads_50"
-fasta_out="../reads/fasta_sim_50.fasta"
+dir_reads=Path(READS_DIR)/"reads50_simlord"/tag
+metadata_reads=json.load(open(dir_reads/"metadata.json"))
+fasta_out=dir_reads/"fasta_sim_50.fasta"
 
-metadata_reads=json.load(open("../reads/reads_50/metadata.json"))
-dir_reads="../reads/reads_50"
-fasta_out="../reads/fasta_sim_50.fasta"
+metadata_reads=json.load(open(dir_reads/"metadata.json"))
 
 # construct single file
-with open(fasta_out, mode='w') as out_file:
-    counter_reads=0
-    for v,k in metadata_reads.items():
-        print(v)
-        with open(k["path"],'r') as f:
-            # introduce one description line for each read
-            for line in f:
-                # specify number of read introduced in the file (0 based)
-                out_file.write(">S0R"+str(counter_reads)+'\n'+str(line)+'\n')
-                counter_reads+=1
+if __name__=="__main__":
+    with open(fasta_out, mode='w') as out_file:
+        counter_reads=0
+        for v,k in metadata_reads.items():
+            print(v)
+            with open(k["path"],'r') as f:
+                # introduce one description line for each read
+                for line in f:
+                    # specify number of read introduced in the file (0 based)
+                    out_file.write(">S0R"+str(counter_reads)+'\n'+str(line)+'\n')
+                    counter_reads+=1
